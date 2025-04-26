06a47f16b9a3f99d0c34e0ec13b942ecfef2518e
Ricardo <ricardohhammer.meet@gmail.com>
2025-04-26 15:13:37 -0400

    Initial commit

---
+++ ArtistasCuritibaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtistasCuritibaApp
+//
+//  Created by user277150 on 4/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
